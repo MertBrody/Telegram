18ced2427f8530d3ee890037af88545684dada3f
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:04:54 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,39 +1,21 @@
-<?php
-require 'vendor/autoload.php'; // Composer ile yüklenen kütüphaneyi dahil edin
+import requests
 
-$api_key = '6593217730:AAHLNvmAkfTWiE441L8s2ES-ubU0-bJbnEE'; // Telegram Bot API anahtarınızı buraya ekleyin
-$chat_id = 'YOUR_CHAT_ID'; // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
+api_key = 'YOUR_API_KEY'  # Telegram Bot API anahtarınızı buraya ekleyin
+chat_id = 'YOUR_CHAT_ID'  # Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+username = 'kullanici'
+password = 'sifre'
 
-    // Kullanıcı girişi doğrulama işlemini burada gerçekleştirin.
+if username == 'kullanici' and password == 'sifre':
+    text = "Başarılı giriş! Kullanıcı: " + username
+else:
+    text = "Kullanıcı adı veya şifre hatalı."
 
-    // Örnek bir kullanıcı adı ve şifresi
-    $correctUsername = "kullanici";
-    $correctPassword = "sifre";
+url = f'https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={text}'
 
-    if ($username == $correctUsername && $password == $correctPassword) {
-        // Kullanıcı girişi başarılı
+response = requests.get(url)
 
-        // Telegram mesajını gönderme işlemini başlatın
-        $telegram = new Longman\TelegramBot\Telegram($api_key);
-
-        $text = "Başarılı giriş! Kullanıcı: " . $username; // Mesaj metni
-        $content = ['chat_id' => $chat_id, 'text' => $text];
-
-        $result = Request::sendMessage($content);
-
-        if ($result->isOk()) {
-            // Mesaj başarıyla gönderildi.
-            echo "Giriş başarılı. Telegram'a mesaj gönderildi.";
-        } else {
-            // Mesaj gönderme hatası
-            echo "Mesaj gönderme hatası: " . $result->getDescription();
-        }
-    } else {
-        echo "Kullanıcı adı veya şifre hatalı.";
-    }
-}
-?>
+if response.status_code == 200:
+    print("Mesaj başarıyla gönderildi.")
+else:
+    print("Mesaj gönderme hatası.")
