9806c07f3a42f37bfadd183d5dc964aec210e769
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 13:28:11 +0300

    Update TiktokDownloader.php

--- TiktokDownloader.php
+++ TiktokDownloader.php
@@ -1,38 +1,65 @@
 <?php
-echo "TikTok videolarını topluca indirmek için video URL'lerini girin. Her URL'yi girdikten sonra Enter tuşuna basın. İndirme işlemi otomatik olarak başlayacaktır.\n";
+// TikTok video indirme aracı
+// Termux'ta çalıştırılabilir
+// chipslays/tiktok-downloader kütüphanesini kullanır
 
-$downloadUrls = array();
+// tiktok-downloader.php dosyasını indirin
+// [3](https://github.com/chipslays/tiktok-downloader) adresinden indirebilirsiniz
+require 'tiktok-downloader.php';
 
-while (true) {
-    echo "Video URL'sini girin (Çıkmak için 'q' girin ve Enter tuşuna basın):\n";
-    $videoUrl = readline();
+// TikTok video URL'sini alın
+// Kısa veya uzun URL olabilir
+$url = readline("TikTok video URL'sini girin: ");
 
-    if (strtolower($videoUrl) === 'q') {
-        break;
-    }
+// TikTok nesnesi oluşturun
+$tiktok = new TikTok();
 
-    $downloadUrls[] = $videoUrl;
-}
+// URL'yi ayarlayın
+$tiktok->url($url);
 
-foreach ($downloadUrls as $videoUrl) {
-    $videoId = extractVideoId($videoUrl);
-    
-    if ($videoId !== false) {
-        $downloadUrl = "https://www.tiktok.com/node/share/video/{$videoId}";
-        // Buradan video indirme işlemi yapabilirsiniz
-        echo "Video başarıyla indirildi: $downloadUrl\n";
+// Video verilerini alın
+$data = $tiktok->getData();
+
+// Video verilerini yazdırın
+print_r($data);
+
+// Videoyu indirmek için seçenek sunun
+echo "Videoyu indirmek ister misiniz? (E/H): ";
+$choice = readline();
+
+// Seçime göre işlem yapın
+if ($choice == "E" || $choice == "e") {
+    // Filigranlı veya filigransız seçeneği sunun
+    echo "Filigranlı mı filigransız mı indirmek istersiniz? (F/N): ";
+    $type = readline();
+
+    // Tipine göre video URL'sini alın
+    if ($type == "F" || $type == "f") {
+        // Filigranlı video URL'sini alın
+        $video_url = $data["video"]["downloadAddr"];
+    } else if ($type == "N" || $type == "n") {
+        // Filigransız video URL'sini alın
+        $video_url = $data["video"]["playAddr"];
     } else {
-        echo "Geçersiz TikTok video URL'si: $videoUrl\n";
+        // Geçersiz seçim durumunda hata mesajı verin
+        echo "Geçersiz seçim. Lütfen F veya N girin.\n";
+        exit();
     }
-}
 
-function extractVideoId($videoUrl) {
-    $pattern = '/\/video\/(\d+)/';
-    preg_match($pattern, $videoUrl, $matches);
-    if (isset($matches[1])) {
-        return $matches[1];
-    } else {
-        return false;
-    }
+    // Video dosya adını alın
+    $filename = basename($video_url);
+
+    // Videoyu indirin ve kaydedin
+    echo "Videoyu indiriyorum...\n";
+    file_put_contents($filename, file_get_contents($video_url));
+    echo "Videoyu başarıyla indirdim: $filename\n";
+} else if ($choice == "H" || $choice == "h") {
+    // İndirmek istemiyorsa çıkış yapın
+    echo "Tamam, iyi günler.\n";
+    exit();
+} else {
+    // Geçersiz seçim durumunda hata mesajı verin
+    echo "Geçersiz seçim. Lütfen E veya H girin.\n";
+    exit();
 }
 ?>
