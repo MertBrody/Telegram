00c7de91f9ac9cf8c6b81f5fcb8b8b7fe90efe3d
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:08:12 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,21 +1,23 @@
-import requests
+<?php
+$api_key = 'YOUR_API_KEY';  // Telegram Bot API anahtarınızı buraya ekleyin
+$chat_id = 'YOUR_CHAT_ID';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
-api_key = 'YOUR_API_KEY'  # Telegram Bot API anahtarınızı buraya ekleyin
-chat_id = 'YOUR_CHAT_ID'  # Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
+$username = 'kullanici';
+$password = 'sifre';
 
-username = 'kullanici'
-password = 'sifre'
+if ($username == 'kullanici' && $password == 'sifre') {
+    $text = "Başarılı giriş! Kullanıcı: " . $username;
+} else {
+    $text = "Kullanıcı adı veya şifre hatalı.";
+}
 
-if username == 'kullanici' and password == 'sifre':
-    text = "Başarılı giriş! Kullanıcı: " + username
-else:
-    text = "Kullanıcı adı veya şifre hatalı."
+$telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($text);
 
-url = f'https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={text}'
+$response = file_get_contents($telegram_api_url);
 
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("Mesaj başarıyla gönderildi.")
-else:
-    print("Mesaj gönderme hatası.")
+if ($response !== false) {
+    echo "Mesaj başarıyla gönderildi.";
+} else {
+    echo "Mesaj gönderme hatası.";
+}
+?>
