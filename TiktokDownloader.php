302a3f1bfeaa76759a6ffadcb20cf409cb2221a2
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 13:34:39 +0300

    Update TiktokDownloader.php

--- TiktokDownloader.php
+++ TiktokDownloader.php
@@ -1,65 +1,17 @@
 <?php
-// TikTok video indirme aracı
-// Termux'ta çalıştırılabilir
-// chipslays/tiktok-downloader kütüphanesini kullanır
-
-// tiktok-downloader.php dosyasını indirin
-// [3](https://github.com/chipslays/tiktok-downloader) adresinden indirebilirsiniz
-require 'tiktok-downloader.php';
-
-// TikTok video URL'sini alın
-// Kısa veya uzun URL olabilir
-$url = readline("TikTok video URL'sini girin: ");
-
-// TikTok nesnesi oluşturun
-$tiktok = new TikTok();
-
-// URL'yi ayarlayın
-$tiktok->url($url);
-
-// Video verilerini alın
-$data = $tiktok->getData();
-
-// Video verilerini yazdırın
-print_r($data);
-
-// Videoyu indirmek için seçenek sunun
-echo "Videoyu indirmek ister misiniz? (E/H): ";
-$choice = readline();
-
-// Seçime göre işlem yapın
-if ($choice == "E" || $choice == "e") {
-    // Filigranlı veya filigransız seçeneği sunun
-    echo "Filigranlı mı filigransız mı indirmek istersiniz? (F/N): ";
-    $type = readline();
-
-    // Tipine göre video URL'sini alın
-    if ($type == "F" || $type == "f") {
-        // Filigranlı video URL'sini alın
-        $video_url = $data["video"]["downloadAddr"];
-    } else if ($type == "N" || $type == "n") {
-        // Filigransız video URL'sini alın
-        $video_url = $data["video"]["playAddr"];
-    } else {
-        // Geçersiz seçim durumunda hata mesajı verin
-        echo "Geçersiz seçim. Lütfen F veya N girin.\n";
-        exit();
-    }
-
-    // Video dosya adını alın
-    $filename = basename($video_url);
-
-    // Videoyu indirin ve kaydedin
-    echo "Videoyu indiriyorum...\n";
-    file_put_contents($filename, file_get_contents($video_url));
-    echo "Videoyu başarıyla indirdim: $filename\n";
-} else if ($choice == "H" || $choice == "h") {
-    // İndirmek istemiyorsa çıkış yapın
-    echo "Tamam, iyi günler.\n";
-    exit();
-} else {
-    // Geçersiz seçim durumunda hata mesajı verin
-    echo "Geçersiz seçim. Lütfen E veya H girin.\n";
-    exit();
+function download_video($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSLVERSION,CURL_SSLVERSION_DEFAULT);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537');
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
 }
+
+$video_url = 'https://www.tiktok.com/@username/video/videoID'; // TikTok video URL
+$video_data = download_video($video_url);
+
+file_put_contents('video.mp4', $video_data);
 ?>
