295595df239a4bd4797050a1f3318d3f0b76f57b
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:24:46 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -2,16 +2,14 @@
 $api_key = '6593217730:AAHLNvmAkfTWiE441L8s2ES-ubU0-bJbnEE';  // Telegram Bot API anahtarınızı buraya ekleyin
 $chat_id = '6669631502';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
-$username = 'kullanici';
-$password = 'sifre';
-
-if ($username == 'kullanici' && $password == 'sifre') {
-    $text = "Başarılı giriş! Kullanıcı: " . $username;
+// Kullanıcıdan mesaj içeriğini al
+if (isset($_POST['message'])) {
+    $message = $_POST['message'];
 } else {
-    $text = "Kullanıcı adı veya şifre hatalı.";
+    $message = 'Varsayılan mesaj: Kullanıcı tarafından girilen mesaj yok.';
 }
 
-$telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($text);
+$telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
 
 $response = file_get_contents($telegram_api_url);
 
