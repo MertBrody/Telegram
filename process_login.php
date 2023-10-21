70d4e5cc6912ebae507fe032a6db21c4e7767b2a
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:26:30 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -2,11 +2,12 @@
 $api_key = '6593217730:AAHLNvmAkfTWiE441L8s2ES-ubU0-bJbnEE';  // Telegram Bot API anahtarınızı buraya ekleyin
 $chat_id = '6669631502';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
-// Kullanıcıdan mesaj içeriğini al
-if (isset($_POST['message'])) {
-    $message = $_POST['message'];
+// Kullanıcıdan mesajı komut satırından alın
+if (isset($argv[1])) {
+    $message = $argv[1];
 } else {
-    $message = 'Varsayılan mesaj: Kullanıcı tarafından girilen mesaj yok.';
+    echo "Lütfen bir mesaj girin.\n";
+    exit(1);
 }
 
 $telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
@@ -14,8 +15,7 @@ $telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$c
 $response = file_get_contents($telegram_api_url);
 
 if ($response !== false) {
-    echo "Mesaj başarıyla gönderildi.";
+    echo "Mesaj başarıyla gönderildi.\n";
 } else {
-    echo "Mesaj gönderme hatası.";
+    echo "Mesaj gönderme hatası.\n";
 }
-?>
