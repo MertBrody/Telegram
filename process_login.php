e82c64344c49df61d5c53ec90f8a10aeb2ea5a62
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-10-21 12:28:52 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,21 +1,19 @@
 <?php
-$api_key = '6593217730:AAHLNvmAkfTWiE441L8s2ES-ubU0-bJbnEE';  // Telegram Bot API anahtarınızı buraya ekleyin
-$chat_id = '6669631502';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
+$api_key = 'YOUR_API_KEY';  // Telegram Bot API anahtarınızı buraya ekleyin
+$chat_id = 'YOUR_CHAT_ID';  // Mesajı göndereceğiniz hedef chat ID'sini buraya ekleyin
 
-// Kullanıcıdan mesajı komut satırından alın
-if (isset($argv[1])) {
-    $message = $argv[1];
+echo "Mesajınızı girin ve Enter tuşuna basın:\n";
+$message = readline();
+
+if (!empty($message)) {
+    $telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
+    $response = file_get_contents($telegram_api_url);
+
+    if ($response !== false) {
+        echo "Mesaj başarıyla gönderildi.\n";
+    } else {
+        echo "Mesaj gönderme hatası.\n";
+    }
 } else {
-    echo "Lütfen bir mesaj girin.\n";
-    exit(1);
-}
-
-$telegram_api_url = "https://api.telegram.org/bot$api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
-
-$response = file_get_contents($telegram_api_url);
-
-if ($response !== false) {
-    echo "Mesaj başarıyla gönderildi.\n";
-} else {
-    echo "Mesaj gönderme hatası.\n";
+    echo "Boş bir mesaj gönderilemez.\n";
 }
