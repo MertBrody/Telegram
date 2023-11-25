d3da2196680ffa142d74508799cf02242436c397
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-11-25 22:50:38 +0300

    Update Telegram.php

--- Telegram.php
+++ Telegram.php
@@ -3,12 +3,23 @@ $token = "6519976251:AAFYccpWtDs5p-lMJKteyquiEEPFtN1ouak";
 $chat_id = "6225633981";
 $message = "Merhaba, bu bir test mesajıdır.";
 
-$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$message}";
+$api_url = "https://api.telegram.org/bot{$token}/sendMessage";
+$params = [
+    'chat_id' => $chat_id,
+    'text' => $message,
+];
 
-$ch = curl_init($url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
 
-echo "Başarıyla kaos ekildi!";
+if ($response === false) {
+    echo "Bağlantı hatası! Şeytanın gücü belki de zayıfladı...";
+} else {
+    echo "Başarıyla kaos ekildi!";
+}
 ?>
