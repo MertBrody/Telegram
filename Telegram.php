d81a294a0f83394a84b0322662e38d2cdf783ec0
MertBrody <148619066+MertBrody@users.noreply.github.com>
2023-11-25 22:59:50 +0300

    Update Telegram.php

--- Telegram.php
+++ Telegram.php
@@ -10,7 +10,7 @@ $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'  => 'POST',
-        'content' => http_build_query($data),
+        'content' => json_encode($data), // Değişiklik burada
     ),
 );
 
